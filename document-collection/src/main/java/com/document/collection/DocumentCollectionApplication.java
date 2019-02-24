8ca11fe315f1bf5e6a133d4def23966772f02710
osmangoni118 <mdosmangoni118@gmail.com>
2019-02-25 03:43:30 +0600

    angular project upload

--- document-collection/src/main/java/com/document/collection/DocumentCollectionApplication.java
+++ document-collection/src/main/java/com/document/collection/DocumentCollectionApplication.java
@@ -2,12 +2,24 @@ package com.document.collection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.*;
 
 @SpringBootApplication
 public class DocumentCollectionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DocumentCollectionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentCollectionApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 
 }
